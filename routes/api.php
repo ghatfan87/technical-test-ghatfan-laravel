6384d97530869ce6fb0612dce7b513263373e1a0
ghatfan87 <muhammadgathfanfadhilhaq@smkwikrama.sch.id>
2024-12-20 10:07:06 +0700

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -17,21 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->group(function (){
-    Route::post('/register',[AuthController::class,'register']);
-    Route::post('/login',[AuthController::class,'login']);
-});
+// Route::prefix('v1')->group(function (){
+//     Route::post('/register',[AuthController::class,'register']);
+//     Route::post('/login',[AuthController::class,'login']);
+// });
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('checklists', ChecklistController::class);
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::apiResource('checklists', ChecklistController::class);
 
-    //checklist item
-    Route::prefix('checklists/{checklist}')->group(function () {
-        Route::get('/items', [ChecklistItemController::class, 'index']);
-        Route::post('/items', [ChecklistItemController::class, 'store']);
-        Route::get('/items/{item}', [ChecklistItemController::class, 'show']);
-        Route::put('/items/{item}', [ChecklistItemController::class, 'update']);
-        Route::patch('/items/{item}/status', [ChecklistItemController::class, 'updateStatus']);
-        Route::delete('/items/{item}', [ChecklistItemController::class, 'destroy']);
-    });
-});
+//     //checklist item
+//     Route::prefix('checklists/{checklist}')->group(function () {
+//         Route::get('/items', [ChecklistItemController::class, 'index']);
+//         Route::post('/items', [ChecklistItemController::class, 'store']);
+//         Route::get('/items/{item}', [ChecklistItemController::class, 'show']);
+//         Route::put('/items/{item}', [ChecklistItemController::class, 'update']);
+//         Route::patch('/items/{item}/status', [ChecklistItemController::class, 'updateStatus']);
+//         Route::delete('/items/{item}', [ChecklistItemController::class, 'destroy']);
+//     });
+// });
--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,21 @@ use App\Http\Controllers\ChecklistController;
 use App\Http\Controllers\ChecklistItemController;
 use Illuminate\Support\Facades\Route;
 
-// Route::prefix('v1')->group(function (){
-//     Route::post('/register',[AuthController::class,'register']);
-//     Route::post('/login',[AuthController::class,'login']);
-// });
+Route::prefix('v1')->group(function (){
+    Route::post('/register',[AuthController::class,'register']);
+    Route::post('/login',[AuthController::class,'login']);
+});
 
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::apiResource('checklists', ChecklistController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('checklists', ChecklistController::class);
 
-//     //checklist item
-//     Route::prefix('checklists/{checklist}')->group(function () {
-//         Route::get('/items', [ChecklistItemController::class, 'index']);
-//         Route::post('/Createitems', [ChecklistItemController::class, 'store']);
-//         Route::get('/items/{item}', [ChecklistItemController::class, 'show']);
-//         Route::put('/items/{item}', [ChecklistItemController::class, 'update']);
-//         Route::patch('/items/{item}/status', [ChecklistItemController::class, 'updateStatus']);
-//         Route::delete('/items/{item}', [ChecklistItemController::class, 'destroy']);
-//     });
-// });
+    //checklist item
+    Route::prefix('checklists/{checklist}')->group(function () {
+        Route::get('/items', [ChecklistItemController::class, 'index']);
+        Route::post('/Createitems', [ChecklistItemController::class, 'store']);
+        Route::get('/items/{item}', [ChecklistItemController::class, 'show']);
+        Route::put('/items/{item}', [ChecklistItemController::class, 'update']);
+        Route::patch('/items/{item}/status', [ChecklistItemController::class, 'updateStatus']);
+        Route::delete('/items/{item}', [ChecklistItemController::class, 'destroy']);
+    });
+});
